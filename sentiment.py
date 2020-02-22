c4db9ad6295103993eb4045b01f773421ecf47f6
lauravillalba <francisco.naranjorodriguez@gmail.com>
2020-02-22 22:59:00 +0100

    POST-GET

--- sentiment.py
+++
@@ -1,7 +0,0 @@
-from bson.json_util import dumps
-from pymongo import MongoClient
-
-def sentimentAnalysis(scenename):
-    
-    
-    return dumps(coll_dialogues.find({'sceneName': scenename}, {'_id': 0, 'userID':1, 'userName':1,'sceneID':1, 'sceneName':1, 'dialogID':1, 'dialog':1}))
